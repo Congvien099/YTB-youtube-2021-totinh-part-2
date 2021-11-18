def82a77f119d62353538aa5fc46701e4980df43
Congvien099 <94543198+Congvien099@users.noreply.github.com>
2021-11-18 09:12:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `mình có điều này muốn hỏi bạn, bạn nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'bạn bị điên phải không? 😙',
+    desc: 'bạn không trả lời hoặc thoát ra, tức là bị điên đó nha :> ',
+    btnYes: 'đúng vậy 💖💖💖',
+    btnNo: '💔 không nha! 💔 ',
+    question:'Nói cho mình lý do đi 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/vien.cong.100'
+    reply: 'vì mình bị điên 😊😊',
+    mess: 'mình biết mà,',
+    messDesc: 'bạn bị điên hả. 💬💬',
+    btnAccept: 'đúng rồi',
+  
 }
